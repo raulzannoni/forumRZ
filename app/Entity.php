9b012766f92492804463624da528514b70157cae
raul.zannoni <raul.zannoni92@gmail.com>
2023-06-27 15:56:01 +0200

    mvc 1st commit

--- app/Entity.php
+++ app/Entity.php
@@ -20,7 +20,7 @@
                 }
                 //fabrication du nom du setter à appeler (ex: setMarque)
                 $method = "set".ucfirst($fieldArray[0]);
-               
+        
                 if(method_exists($this, $method)){
                     $this->$method($value);
                 }
