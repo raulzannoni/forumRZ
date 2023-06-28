ff3ae5bd4fdef55b8dc44dda686a00872fa25075
raul_zannoni <raul.zannnoni92@gmail.com>
2023-06-28 15:33:23 +0200

    DAO study

--- app/DAO.php
+++ app/DAO.php
@@ -13,7 +13,7 @@
     abstract class DAO{
 
         private static $host   = 'mysql:host=127.0.0.1;port=3306';
-        private static $dbname = 'forum_dwwm3';
+        private static $dbname = 'forumRZ';
         private static $dbuser = 'root';
         private static $dbpass = '';
 
@@ -36,19 +36,22 @@
             );
         }
 
-        public static function insert($sql){
-            try{
-                $stmt = self::$bdd->prepare($sql);
-                $stmt->execute();
-                //on renvoie l'id de l'enregistrement qui vient d'être ajouté en base, 
-                //pour s'en servir aussitôt dans le controleur
-                return self::$bdd->lastInsertId();
-                
+        public static function insert($sql)
+            {
+                try
+                    {
+                        $stmt = self::$bdd->prepare($sql);
+                        $stmt->execute();
+
+                        //on renvoie l'id de l'enregistrement qui vient d'être ajouté en base, 
+                        //pour s'en servir aussitôt dans le controleur
+                        return self::$bdd->lastInsertId();
+                    }
+                catch(\Exception $e)
+                    {
+                        echo $e->getMessage();
+                    }
             }
-            catch(\Exception $e){
-                echo $e->getMessage();
-            }
-        }
 
         public static function update($sql, $params){
             try{
@@ -64,20 +67,23 @@
             }
         }
         
-        public static function delete($sql, $params){
-            try{
-                $stmt = self::$bdd->prepare($sql);
-                
-                //on renvoie l'état du statement après exécution (true ou false)
-                return $stmt->execute($params);
-                
+        public static function delete($sql, $params)
+            {
+                try
+                    {
+                        $stmt = self::$bdd->prepare($sql);
+                        
+                        //on renvoie l'état du statement après exécution (true ou false)
+                        return $stmt->execute($params);
+                        
+                    }
+                catch(\Exception $e)
+                    {
+                        echo $sql;
+                        echo $e->getMessage();
+                        die();
+                    }
             }
-            catch(\Exception $e){
-                echo $sql;
-                echo $e->getMessage();
-                die();
-            }
-        }
 
         /**
          * Cette méthode permet les requêtes de type SELECT
@@ -89,18 +95,21 @@
          * @return array|null les enregistrements en FETCH_ASSOC ou null si aucun résultat
          */
         public static function select($sql, $params = null, bool $multiple = true):?array
-        {
-            try{
-                $stmt = self::$bdd->prepare($sql);
-                $stmt->execute($params);
-              
-                $results = ($multiple) ? $stmt->fetchAll() : $stmt->fetch();
+            {
+                try
+                    {
+                        $stmt = self::$bdd->prepare($sql);
+                        $stmt->execute($params);
+                    
+                        $results = ($multiple) ? $stmt->fetchAll() : $stmt->fetch();
 
-                $stmt->closeCursor();
-                return ($results == false) ? null : $results;
+                        $stmt->closeCursor();
+                        
+                        return ($results == false) ? null : $results;
+                    }
+                catch(\Exception $e)
+                    {
+                        echo $e->getMessage();
+                    }
             }
-            catch(\Exception $e){
-                echo $e->getMessage();
-            }
-        }
     }
